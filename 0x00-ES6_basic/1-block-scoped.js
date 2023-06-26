f6bdbb3b2d2609ad5cbc45e8423c40ee0ea751d3
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-06-26 14:38:49 +0000

    fix all errors

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,7 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
-
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    return [false, true];
   }
 
-  return [task, task2];
+  return [false, true];
 }
