fb06c475a457e9a804ff198d9413230e7b45d71b
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-08-22 10:08:35 +0000

    task 5 - 8

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,11 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!\n');
 });
 
-app.listen(1245)
-
+app.listen(1245);
 
 module.exports = app;
