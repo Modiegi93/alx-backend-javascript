d1c6f23c8d47e815f51b52913ef2186f15ff4de0
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-08-24 09:19:52 +0200

    task 0 -5

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,9 @@
+// Calculation function
+
+function calculateNumber(a, b) {
+  roundedA = Math.round(a);
+  roundedB = Math.round(b);
+  return roundedA + roundedB;
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,21 @@
+const Utils = {
+    calculateNumber(type, a, b) {
+    const roundedA = Math.round(a);
+    const roundedB = Math.round(b);
+
+    if (type === 'SUBTRACT') {
+       return roundedA - roundedB;
+    }
+
+    if (type === 'DIVIDE') {
+        if (roundedB === 0) {
+        return 'Error';
+        }
+        return roundedA / roundedB;
+    }
+
+    return roundedA + roundedB;
+    }
+};
+
+module.exports = Utils;
